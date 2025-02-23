0b093fe0568e52c07514a9422bc5d97f5f99e326
Suhard Damien <ptitdam2001@gmail.com>
2025-02-23 21:29:45 +0100

    build(Frontend): Update Dependancies + openapi update

--- frontend/Application-material/orval.config.ts
+++ frontend/Application-material/orval.config.ts
@@ -18,16 +18,36 @@ export default {
           countTeams: {
             mock: {
               data: faker.number.int({ min: 10, max: 250 }),
-
             },
           },
+          getTeamPlayers: {
+            mock: {
+              arrayMin: 10,
+              arrayMax: 25,
+            }
+          },
+          getTeam: {
+            mock: {
+              arrayMin: 10,
+              arrayMax: 50,
+            }
+          }
         },
         mock: {
           properties: {
-            '/name/': () => faker.person.fullName(),
-            '/color/': () => faker.color.rgb(),
+            name: () => faker.person.fullName(),
+            '/jersey/': () => faker.number.int({ min: 0, max: 99 }),
+            '/avatar/': () => faker.helpers.arrayElement([faker.image.personPortrait(), undefined]),
+
+          },
+          format: {
+            color: () => faker.color.rgb(),
+            score: () => faker.number.int({ min: 0, max: 60})
           },
           delay: 500,
+          delayFunctionLazyExecute: true,
+          arrayMin: 6,
+          arrayMax: 25,
         },
       },
       // mock: {
@@ -38,6 +58,7 @@ export default {
       mock: true,
       mode: 'split',
       allParamsOptional: true,
+      prettier: true,
     },
     hooks: {
       afterAllFilesWrite: 'prettier --write',
