5d7a001d3de496f4022c27164554f7696a318b17
Suhard Damien <ptitdam2001@gmail.com>
2025-04-03 07:08:21 +0200

    docs(Storybook): Upgrade storybook

--- frontend/Application-material/.storybook/main.ts
+++ frontend/Application-material/.storybook/main.ts
@@ -5,16 +5,28 @@ const config: StorybookConfig = {
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-mdx-gfm"
   ],
+
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
   },
-  staticDirs: ['../public', '../static'],
+
+  staticDirs: ['../public'],
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
